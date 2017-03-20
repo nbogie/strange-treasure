04b1e3570a5284780899028766245f44be6bfb84
Neill Bogie <neillbogie@googlemail.com>
2017-03-20 02:48:11 +0000

    more words

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ console.log("my tracery demo");
 $(document).ready(function () {
 
     var grammar = {
-        "origin": ["#normalTreasure#"],
-        "treasure": ["#normalTreasure#", "#normalTreasurePl#", "#liquidTreasure#", "#amberTreasure#"], 
+        "origin": ["#treasure#"],
+        "treasure": ["#normalTreasure#", "#normalTreasurePl#", "#liquidTreasure#", "#amberTreasure#", "#twoNouns#"], 
+        "twoNouns": ["A #singthing# #singthing#"],
         "normalTreasure": ["A #magicAdj# #magicNounOrNot# #singthing#"],
         "normalTreasurePl": ["A #container# of #magicAdj# #magicNounOrNot# #singthing.s#"],
         "amberTreasure": ["A #caughtThing# encased in amber"],
@@ -22,8 +23,8 @@ $(document).ready(function () {
         "liquidThings": ["#allCreature# blood", "#allCreature# tears", "#mythBeast# ichor"],
         "liquidPrep": " | | | | | | | | | |reduced|concentrated|boiled|evaporated|distilled|condensed|frozen".split("|"),
         "allCreature": ["#animal#", "#mythBeast#"],
-        "magicAdj": "blood-drinking|murmuring|ancient|royal|ethereal|subliminal|sidhe|unspoken|unthere|unholy|glitching|whispering|blood-stained|magical|fae|cursed|dark|divine|celestial|cold".split("|"),
-        "singthing": "light|crossbow|snow globe|love letter|letter|treasure map|brooch|map|saddle|music box|miniature chest|children's book|monacle|toy soldier|crystal|egg|breastplate|painting|earring|tiara|crown|animal statue|doll|deed|bird-cage|head|manual|relic|flower|wedding dress|chess set|puzzle box|crystal ball|straight-razor|journal|snare|dagger|statuette|symbol|cloak|riddle|word|blessing|mask|pendant|mirror|horn|gem|garland|pebble|harp|petal|demon|bean|potion|#promiseType# promise|true name|portal|scroll|coin".split("|"),
+        "magicAdj": "forgotten|sentient|possessed|flying|gilded|tiny|holy|well-travelled|historied|phasing|vibrating|shimmering|blood-drinking|murmuring|ancient|royal|ethereal|subliminal|sidhe|unspoken|unthere|unholy|glitching|whispering|blood-stained|magical|fae|cursed|dark|divine|celestial|cold".split("|"),
+        "singthing": "wand|apron|costume|necklace|helm|whistle|seed|fan|tooth|skull|finger|root|anger|lust|kiss|joke|heart|rumour|whisper|snowflake|light|crossbow|snow globe|love letter|letter|treasure map|brooch|map|saddle|music box|miniature chest|children's book|monacle|toy soldier|crystal|egg|breastplate|painting|earring|tiara|crown|animal statue|doll|deed|bird-cage|head|manual|relic|flower|wedding dress|chess set|puzzle box|crystal ball|straight-razor|journal|snare|dagger|statuette|symbol|cloak|riddle|word|blessing|mask|pendant|mirror|horn|gem|garland|pebble|harp|petal|demon|bean|potion|#promiseType# promise|true name|portal|scroll|coin".split("|"),
         "magicNounOrNot": ["", "#magicNoun#"],
         "magicNoun": "time|demon|fairy|star|#mythBeast#".split("|"), 
         "mythBeast": "sprite|wisp|leprechaun|ogre|troll|vampire|werewolf|ghost|mermaid|fairy|pixie|satyr|dryad|cheshire cat".split("|"),
